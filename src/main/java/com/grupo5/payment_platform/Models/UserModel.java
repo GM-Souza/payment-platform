cee6ef74e944b7c5da1728838b32fb57ce894497
GM-Souza <guilhermematos2313@gmail.com>
2025-09-23 11:25:40 -0300

    feat: Create UserModel

---
+++ src/main/java/com/grupo5/payment_platform/Models/UserModel.java
@@ -0,0 +1,13 @@
+package com.grupo5.payment_platform.Models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_usuario")
+public class UserModel {
+
+    private Long id;
+
+}
