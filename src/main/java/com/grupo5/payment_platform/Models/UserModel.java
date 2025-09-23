01ee99442e69ba309eeb9c5e60cefb6d0fd91dd8
Fabricio Moraes <Fabricio36365@outlook.com>
2025-09-23 18:19:52 -0300

    Fix: Classe mãe para consistência do relacionamento no banco de dados

--- src/main/java/com/grupo5/payment_platform/Models/UserModel.java
+++ src/main/java/com/grupo5/payment_platform/Models/UserModel.java
@@ -1,13 +1,32 @@
 package com.grupo5.payment_platform.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name="tb_usuario")
+@Table(name="tb_users")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class UserModel {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
+    @Column(name = "balance")
+    private BigDecimal balance = BigDecimal.ZERO;
+
 
 }
