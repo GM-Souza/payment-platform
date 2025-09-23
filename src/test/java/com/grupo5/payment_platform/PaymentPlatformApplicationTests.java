35968d31a5fb28e4e1b96a6a815f14cafe652774
GM-Souza <guilhermematos2313@gmail.com>
2025-09-23 11:05:28 -0300

    feat: First commit

---
+++ src/test/java/com/grupo5/payment_platform/PaymentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.payment_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
