b33dbf6a8cdee2e6a93aa30efe11c820e03c4a21
Fabricio Moraes <Fabricio36365@outlook.com>
2025-09-29 15:45:18 -0300

    Feat: criação do endpoint para pagamento pix

--- src/main/java/com/grupo5/payment_platform/Controllers/TransactionController.java
+++ src/main/java/com/grupo5/payment_platform/Controllers/TransactionController.java
@@ -1,9 +1,11 @@
 package com.grupo5.payment_platform.Controllers;
 
 
+import com.grupo5.payment_platform.DTOs.PixResponseDTO;
 import com.grupo5.payment_platform.DTOs.TransactionRequestDTO;
 import com.grupo5.payment_platform.DTOs.TransactionResponseDTO;
 import com.grupo5.payment_platform.Models.TransactionModel;
+import com.grupo5.payment_platform.Models.payments.PixPaymentDetail;
 import com.grupo5.payment_platform.Services.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +24,20 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    //Endpoint para metodo de transferencia interna p2p
     @PostMapping
     public ResponseEntity<TransactionModel> createTransaction(@RequestBody TransactionRequestDTO dto) throws Exception {
         TransactionModel newTransaction = this.transactionService.createTransaction(dto);
         return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+
+    //Endpoint para metodo de transferencia pix via mp
+    @PostMapping("/pix")
+    public ResponseEntity<PixResponseDTO> createPixTransaction(@RequestBody TransactionRequestDTO dto) throws Exception {
+       TransactionModel pixTransaction = transactionService.createPixTransaction(dto);
+
+        PixPaymentDetail pixPaymentDetail = (PixPaymentDetail)  pixTransaction.getPaymentDetail();
+        PixResponseDTO pixResponse = new PixResponseDTO(pixTransaction.getId().toString(),pixTransaction.getStatus().toString(),pixPaymentDetail.getQrCodeBase64(),pixPaymentDetail.getQrCodeCopyPaste());
+        return ResponseEntity.ok(pixResponse);
     }
 }
