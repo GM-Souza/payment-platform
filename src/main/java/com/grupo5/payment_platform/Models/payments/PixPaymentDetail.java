8d50bc89e32f79fec3c6a2f8f3893c538f80ad0a
Your Name <you@example.com>
2025-10-01 08:39:48 -0300

    Fix: Correção do pagamento pix para gerar o QrCode e efetuar o pagamento

--- src/main/java/com/grupo5/payment_platform/Models/payments/PixPaymentDetail.java
+++ src/main/java/com/grupo5/payment_platform/Models/payments/PixPaymentDetail.java
@@ -7,11 +7,16 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "tb_pix_payment_details")
 @Getter
 @Setter
 public class PixPaymentDetail extends PaymentDetail {
+
+    private Long mercadoPagoPaymentId; // ID do pagamento gerado pelo MercadoPago
+    private BigDecimal amount;
 
 
     @Column(name = "qr_code_base64", columnDefinition = "TEXT")
--- src/main/java/com/grupo5/payment_platform/Repositories/PixPaymentDetailRepository.java
+++ src/main/java/com/grupo5/payment_platform/Repositories/PixPaymentDetailRepository.java
@@ -1,10 +1,11 @@
 package com.grupo5.payment_platform.Repositories;
 
-import com.grupo5.payment_platform.Models.TransactionModel;
 import com.grupo5.payment_platform.Models.payments.PixPaymentDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TransactionRepository extends JpaRepository<TransactionModel, UUID> {
+public interface PixPaymentDetailRepository extends JpaRepository<UUID, PixPaymentDetail> {
+    static PixPaymentDetail findByQrCodeCopyPaste(String qrCodeCopyPaste);
+
 }
