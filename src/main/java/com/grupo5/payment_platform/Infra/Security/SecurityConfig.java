a7d1313393b4d10ccf9e7a0d96251f1c649af15d
Fabricio Moraes <Fabricio36365@outlook.com>
2025-10-05 12:54:25 -0300

    Feat: Criação filtro para os endpoints

---
+++ src/main/java/com/grupo5/payment_platform/Infra/Security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.grupo5.payment_platform.Infra.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+   private SecurityFilter securityFilter;
+   private CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(SecurityFilter securityFilter, CustomUserDetailsService customUserDetailsService) {
+        this.securityFilter = securityFilter;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/auth/register-individual").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/auth/register-legalentity").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/auth/logout").permitAll()
+                        .requestMatchers(HttpMethod.DELETE,"/auth").hasRole("ADMIN")
+                        .requestMatchers("/users/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+    //Bean para criptografia de senha
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
