d1d2c376288250e25047fb4934e032727b9727e7
Fabricio Moraes <Fabricio36365@outlook.com>
2025-10-05 12:50:13 -0300

    Feat: Add o metodo de findByEmail

--- src/main/java/com/grupo5/payment_platform/Repositories/UserRepository.java
+++ src/main/java/com/grupo5/payment_platform/Repositories/UserRepository.java
@@ -3,8 +3,11 @@ package com.grupo5.payment_platform.Repositories;
 import com.grupo5.payment_platform.Models.Users.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
     UserModel findUserById(UUID id);
+
+    Optional<UserModel> findByEmail(String login);
 }
