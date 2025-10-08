b787349916ca3b4bf032b93734b316c8a804d177
Guilherme Souza <guilhermematos2313@gmail.com>
2025-10-08 19:35:22 -0300

    Fix: remoção de método conflitante. OBS: reformular método para gerar comprovante

--- src/main/java/com/grupo5/payment_platform/Repositories/TransactionRepository.java
+++ src/main/java/com/grupo5/payment_platform/Repositories/TransactionRepository.java
@@ -2,20 +2,14 @@ package com.grupo5.payment_platform.Repositories;
 
 import com.grupo5.payment_platform.Enums.TransactionStatus;
 import com.grupo5.payment_platform.Models.Payments.TransactionModel;
-import com.grupo5.payment_platform.Models.Users.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<TransactionModel, UUID> {
-   List<TransactionModel> findByStatusAndDateBefore(TransactionStatus transactionStatus, LocalDateTime limite);
 
-    List<TransactionModel> findByStatusAndCreateDateBefore(TransactionStatus transactionStatus, LocalDateTime limite);
-
-    //Metodo para listar todas as transações de um usuário, seja como remetente ou destinatário
-    List<TransactionModel> findBySenderIdOrReceiverId(UUID sender,UUID receiver);
-
+    // Consulta válida usando campo existente 'date'
+    List<TransactionModel> findByStatusAndDateBefore(TransactionStatus transactionStatus, LocalDateTime limite);
 }
