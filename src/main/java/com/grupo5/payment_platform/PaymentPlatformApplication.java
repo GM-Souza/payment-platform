ff517bda8701d1efac2f1eb514486a0c626cf6a4
Guilherme Souza <guilhermematos2313@gmail.com>
2025-10-14 20:12:43 -0300

    Fix: agora o usuário é salvo corretamente

--- src/main/java/com/grupo5/payment_platform/PaymentPlatformApplication.java
+++ src/main/java/com/grupo5/payment_platform/PaymentPlatformApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// teste de .JAR
 @EnableScheduling
 @SpringBootApplication
 public class PaymentPlatformApplication {
