9acbbc4653ba709e9a018098115c9447c39ead93
junior12577 <juninhooi375@gmail.com>
2025-10-16 15:20:08 -0300

    ADD Link Front

--- src/main/java/com/grupo5/payment_platform/Infra/Security/SecurityConfig.java
+++ src/main/java/com/grupo5/payment_platform/Infra/Security/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081")); // frontend local
+        configuration.setAllowedOrigins(Arrays.asList("https://front-agi-pay.vercel.app/")); // frontend local
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true); // importante para JWT ou cookies
